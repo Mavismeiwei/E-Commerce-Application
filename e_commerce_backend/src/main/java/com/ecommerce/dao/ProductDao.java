c20a0123a02b68d2bef6d8f4057778ab4ca64fc9
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-01-22 14:05:07 -0500

    Implement pagination on home page

--- e_commerce_backend/src/main/java/com/ecommerce/dao/ProductDao.java
+++ e_commerce_backend/src/main/java/com/ecommerce/dao/ProductDao.java
@@ -1,8 +1,12 @@
 package com.ecommerce.dao;
 
 import com.ecommerce.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ProductDao extends CrudRepository<Product, Integer> {
 
+    public List<Product> findAll(Pageable pageable);
 }
