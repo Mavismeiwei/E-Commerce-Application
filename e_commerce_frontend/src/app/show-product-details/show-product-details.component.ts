2592adf837b2d01d9884e9393f7dd2ec33bdecbd
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-01-23 11:14:01 -0500

    Implement pagination on Show Product Details (admin)

--- e_commerce_frontend/src/app/show-product-details/show-product-details.component.ts
+++ e_commerce_frontend/src/app/show-product-details/show-product-details.component.ts
@@ -15,6 +15,10 @@ import { Router } from '@angular/router';
 })
 export class ShowProductDetailsComponent implements OnInit {
 
+  showLoadMoreProductButton = false;
+
+  showTable = false;
+  pageNumber: number = 0;
   productDetails: Product[] = [];
   displayedColumns: string[] = ['ID', 'Product Name', 'description', 'Product Discounted Price', 'Product Actual Price', 'Actions'];
 
@@ -28,18 +32,31 @@ export class ShowProductDetailsComponent implements OnInit {
   }
 
   public getAllProducts(){
-    this.productService.getAllProducts(0)
+    this.showTable = false;
+    this.productService.getAllProducts(this.pageNumber)
     .pipe(
      map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
     )
     .subscribe(
       (resp: Product[]) => {
-        console.log(resp);
-        this.productDetails = resp;
+        resp.forEach(product => this.productDetails.push(product));
+        this.showTable = true;
+
+        if(resp.length == 8) {
+          this.showLoadMoreProductButton = true;
+        } else {
+          this.showLoadMoreProductButton = false;
+        }
+
       }, (error: HttpErrorResponse) => {
         console.log(error);
       }
     );
+  }
+
+  loadMoreProduct() {
+    this.pageNumber = this.pageNumber + 1;
+    this.getAllProducts();
   }
 
   deleteProduct(productId: number) {
