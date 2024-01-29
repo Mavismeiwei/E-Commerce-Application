0e3e6ddf2dfbd90d54475165ce49d2f2b04c8955
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-01-28 19:18:20 -0500

    Update shopping cart

--- e_commerce_frontend/src/app/cart/cart.component.ts
+++ e_commerce_frontend/src/app/cart/cart.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../_services/product.service';
-import { Route, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../_model/product.model';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ImageProcessingService } from '../image-processing.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
+
 export class CartComponent implements OnInit {
 
-  displayedColumns: string[] = ['Name', 'Description', 'Price', 'Discounted Price', 'Action'];
+  displayedColumns: string[] = ['Name', 'Discounted Price', 'Image', 'Action'];
 
   cartDetails: any[] = [];
+  product: Product;
 
   constructor(private productService: ProductService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer
     ) { }
 
   ngOnInit(): void {
+    this.product = this.activatedRoute.snapshot.data['product'];
     this.getCartDetails();
   }
 
   getCartDetails() {
-    this.productService.getCartDetails().subscribe(
+    this.productService.getCartDetails()
+    .subscribe(
       (response: any[]) => {
         console.log(response);
+        console.log(this.product)
         this.cartDetails = response;
       },
       (error) => {
@@ -33,6 +44,12 @@ export class CartComponent implements OnInit {
     );
   }
 
+  getSanitizedImageUrl(base64Data: string) {
+    const imageUrl = 'data:image/webp;base64,' + base64Data; 
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  
   checkout() {
     this.router.navigate(['/buyProduct', {
       isSingleProductCheckout: false, id: 0
